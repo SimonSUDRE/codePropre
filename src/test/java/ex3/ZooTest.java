5696f6339cc802ab738227b6bd0e44b14dd6d222
SimonSUDRE <sta.sudre@gmail.com>
2018-01-03 12:45:18 +0100

    test

--- src/test/java/ex3/ZooTest.java
+++ src/test/java/ex3/ZooTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
-class ZooTest {
+public class ZooTest {
 
 	private Zoo zoo;
 	
@@ -37,10 +37,10 @@ class ZooTest {
 		zoo.addAnimal(new Poisson("Truite doree", "HERBIVORE"));
 		zoo.addAnimal(new Reptile("Boa constrictor", "CARNIVORE"));
 		
-		assertTrue(zoo.getZoneByType(TypeZone.SAVANE_AFRICAINE).getNbAnimaux() == 1);
-		assertTrue(zoo.getZoneByType(TypeZone.AQUARIUM).getNbAnimaux() == 1);
-		assertTrue(zoo.getZoneByType(TypeZone.FERME_REPTILE).getNbAnimaux() == 1);
-		assertTrue(zoo.getZoneByType(TypeZone.ZONE_CARNIVORE).getNbAnimaux() == 1);
+		assertEquals(zoo.getZoneByType(TypeZone.SAVANE_AFRICAINE).getNbAnimaux(), 1);
+		assertEquals(zoo.getZoneByType(TypeZone.AQUARIUM).getNbAnimaux(), 1);
+		assertEquals(zoo.getZoneByType(TypeZone.FERME_REPTILE).getNbAnimaux(), 1);
+		assertEquals(zoo.getZoneByType(TypeZone.ZONE_CARNIVORE).getNbAnimaux(), 1);
 	}
 	
 	@Test
